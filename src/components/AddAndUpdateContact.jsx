edc8fda026e15235352bb297779bf079b5bbe1ce
Rogue-strider <satyamjha91064@gmail.com>
2025-03-30 12:37:02 +0530

    installing formik and making form UI

--- src/components/AddAndUpdateContact.jsx
+++ src/components/AddAndUpdateContact.jsx
@@ -1,11 +1,26 @@
 import React from "react";
 import Modal from "./Modal";
+import { Field, Form, Formik } from "formik";
 
-const AddAndUpdateContact = ({isOpen, onClose}) => {
+const AddAndUpdateContact = ({ isOpen, onClose }) => {
   return (
     <div>
       <Modal isOpen={isOpen} onClose={onClose}>
-        Hi
+        <Formik>
+          <Form className="flex flex-col gap-4">
+            <div className="flex flex-col gap-1">
+              <label htmlFor="name">Name</label>
+              <Field name="name" className="border h-10"/>
+            </div>
+            <div className="flex flex-col gap-1">
+              <label htmlFor="email">Email</label>
+              <Field type="email" name="email" className="border h-10"/>
+            </div>
+            <button className="border bg-[#F6820C] px-3 py-1.5 self-end">
+                add contact
+            </button>
+          </Form>
+        </Formik>
       </Modal>
     </div>
   );
